b5a654fcf62173e8f191f6c5af8ab4dc1b883aac
Martin Frisk <yfflan@gmail.com>
2024-04-05 23:19:26 +0200

    some nice-to-haves while im working

---
+++ alpha_cc/dev_tools/decorators.py
@@ -0,0 +1,14 @@
+from collections.abc import Callable
+from typing import Any, TypeVar
+
+from alpha_cc.dev_tools.timer import Timer
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+
+def dbg_timer(func: F) -> F:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        with Timer(func.__name__, verbose=True):
+            return func(*args, **kwargs)
+
+    return wrapper  # type: ignore  # i tried :-(
---
+++ alpha_cc/dev_tools/timer.py
@@ -0,0 +1,36 @@
+from time import perf_counter
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class DbgStats(BaseModel):
+    tag: str
+    start_time: float
+    stop_time: float | None = None
+
+    @property
+    def duration(self) -> float | None:
+        if self.stop_time is None:
+            return None
+        return self.stop_time - self.start_time
+
+
+class Timer:
+    def __init__(self, tag: str, verbose: bool = False) -> None:
+        self._tag = tag
+        self._verbose = verbose
+        self._dbg_stats: DbgStats
+        self._start_time: float
+        self._stop_time: float
+
+    def __enter__(self) -> DbgStats:
+        self._start_time = perf_counter()
+        self._dbg_stats = DbgStats(tag=self._tag, start_time=self._start_time)
+        return self._dbg_stats
+
+    def __exit__(self, *_: Any, **__: Any) -> None:
+        self._stop_time = perf_counter()
+        self._dbg_stats.stop_time = self._stop_time
+        if self._verbose:
+            print(f"{self._tag} ran for {self._dbg_stats.duration} seconds")  # noqa
