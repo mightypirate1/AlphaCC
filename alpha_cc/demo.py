875e77e33f989cc29ecfec10ff564e8046cbb233
Martin Frisk <yfflan@gmail.com>
2024-04-07 23:08:21 +0200

    wip

--- alpha_cc/demo.py
+++ alpha_cc/demo.py
@@ -1,5 +1,7 @@
 from alpha_cc.agents.heuristic_agents.greedy_agent import GreedyAgent
+from alpha_cc.agents.mcts.mcts_agent import MCTSAgent
 from alpha_cc.engine import Board
+from alpha_cc.nn.nets.default_net import DefaultNet
 from alpha_cc.runtime.runtime import RunTime, RunTimeConfig
 
 size = 9
@@ -8,7 +10,8 @@ board = Board(size)
 
 agents = (
     GreedyAgent(size),
-    GreedyAgent(size),
+    # GreedyAgent(size),
+    MCTSAgent(DefaultNet()),
 )
 config = RunTimeConfig(
     verbose=True,
@@ -17,4 +20,4 @@ config = RunTimeConfig(
     starting_player=starting_player,
 )
 runtime = RunTime(board, agents, config=config)
-runtime.play_game()
+runtime.play_game(training=False)
