8028b962902a077dfcc49bd6969edb4eefdcf044
lucas <kylberg@gmail.com>
2024-05-09 00:41:47 +0200

    Add sidebar and header with navbar

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BoardComponent } from './board/board.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BoardComponent],
+  imports: [RouterOutlet, BoardComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
