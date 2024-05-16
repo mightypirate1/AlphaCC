c39dcb461c5c736b1dc106bea1accf63193e6561
lucas <kylberg@gmail.com>
2024-05-16 22:52:54 +0200

    refactoring and minor changes

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { BoardComponent } from './game/board/board.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
-    { path: 'home', component: BoardComponent},
-    { path: 'new-game', component: BoardComponent},
-    { path: 'inspect-game', component: BoardComponent},
-    { path: 'about', component: BoardComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: BoardComponent },
+  { path: 'new-game', component: BoardComponent },
+  { path: 'inspect-game', component: BoardComponent },
+  { path: 'about', component: BoardComponent },
 ];
