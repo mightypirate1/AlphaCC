5b17edb9b0de4625a3ddae4789e8781ff924c48b
lucas <kylberg@gmail.com>
2024-05-16 22:55:36 +0200

    add board drag-drop and backend communication

--- webapp/src/app/game/board/board.component.ts
+++ webapp/src/app/game/board/board.component.ts
@@ -1,31 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { BoardService } from '../shared/board.service';
-import { Board } from '../shared/board.model';
-import { HoleComponent } from '../board-hole/board-hole.component';
+import { CommonModule } from '@angular/common';
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+
+import { Board } from '../types/board.model';
+import { Point } from '../types/point.model';
+import { PegDirective } from '../directives/peg.directive';
+import { BoardService } from '../services/board.service';
 
 @Component({
-    selector: 'app-board',
-    standalone: true,
-    templateUrl: './board.component.html',
-    styleUrl: './board.component.scss',
-    imports: [HoleComponent]
+  selector: 'app-board',
+  standalone: true,
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss',
+  imports: [CommonModule, PegDirective, DragDropModule],
 })
-export class BoardComponent implements OnInit{
-
+export class BoardComponent implements OnInit {
   board: Board | undefined;
+  colors = ['', 'orange', 'rebeccapurple'];
+  selected: Point = { x: -1, y: -1 };
 
-  constructor(private boardService: BoardService) { }
+  constructor(private boardService: BoardService) {}
 
   ngOnInit(): void {
-    this.showBoard();
+    this.boardService.getNewGameBoard().subscribe((board: Board) => {
+      this.board = board;
+    });
   }
 
-  showBoard() {
-    this.boardService.getBoard()
-      .subscribe({
-        next: (data) => {
-          this.board = data;
-        }
-      })
+  setSelected(x: number, y: number): void {
+    this.selected = { x: x, y: y };
+  }
+
+  drop(
+    $event: CdkDragDrop<{ pegType: number; row: number; col: number }>
+  ): void {
+    const fromX = $event.item.dropContainer.data.row;
+    const fromY = $event.item.dropContainer.data.col;
+    const toX = $event.container.data.row;
+    const toY = $event.container.data.col;
+    if (this.board?.isLegalMove(fromX, fromY, toX, toY)) {
+      const moveIndex = this.board?.getMoveIndex(fromX, fromY, toX, toY);
+      this.boardService
+        .applyMove(this.board?.gameId, moveIndex)
+        .subscribe((board: Board) => {
+          this.board = board;
+        });
+    }
   }
 }
