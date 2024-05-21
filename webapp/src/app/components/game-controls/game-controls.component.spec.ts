e39c3d7aeb44375e4dfeb4d5518390e79d9026b0
lucas <kylberg@gmail.com>
2024-05-21 23:22:10 +0200

    add game controls component

---
+++ webapp/src/app/components/game-controls/game-controls.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameControlsComponent } from './game-controls.component';
+
+describe('GameControlsComponent', () => {
+  let component: GameControlsComponent;
+  let fixture: ComponentFixture<GameControlsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GameControlsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(GameControlsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
