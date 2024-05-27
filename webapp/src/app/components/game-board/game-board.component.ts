74f180a0dad14e6f2fdb03e6efc8af8c2d68a8f5
lucas <kylberg@gmail.com>
2024-05-27 20:24:40 +0200

    refactoring

--- webapp/src/app/components/game-board/game-board.component.ts
+++ webapp/src/app/components/game-board/game-board.component.ts
@@ -1,27 +1,76 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 
-import { Board } from '../../types/board.model';
-import { Point } from '../../types/point.model';
+import { GameService } from '../../services/game.service';
 import { BoardPegComponent } from '../board-peg/board-peg.component';
+import { Move } from '../../types/move.model';
+import { Point } from '../../types/point.model';
 
 @Component({
   selector: 'app-game-board',
   standalone: true,
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss',
-  imports: [CommonModule, DragDropModule, BoardPegComponent],
+  imports: [AsyncPipe, CommonModule, DragDropModule, BoardPegComponent],
 })
-export class GameBoardComponent {
-  @Input() board: Board | undefined;
-  @Input() moveDisabled: boolean = false;
-  @Output() applyMoveEvent: EventEmitter<number> = new EventEmitter<number>();
+export class GameBoardComponent implements OnDestroy {
   colors = ['', 'orange', 'rebeccapurple'];
   selected: Point = { x: -1, y: -1 };
 
+  private readonly onDestroy = new Subject<void>();
+  currentBoardMatrix$: Observable<number[][]>;
+
+  lastMove: Move = {
+    fromCoord: { x: -1, y: -1 },
+    toCoord: { x: -1, y: -1 },
+    path: [],
+    index: -1,
+  };
+
+  legalMoves: Move[] = [];
+
+  constructor(private gameService: GameService) {
+    this.currentBoardMatrix$ = gameService.getCurrentBoardMatrix();
+
+    gameService
+      .getLastMove()
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe((move) => {
+        if (move !== null) {
+          this.lastMove = move;
+        }
+      });
+    gameService
+      .getLegalMoves()
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe((moves) => {
+        this.legalMoves = moves;
+      });
+  }
+  ngOnDestroy(): void {
+    this.onDestroy.next();
+  }
+
   setSelected(x: number, y: number): void {
     this.selected = { x: x, y: y };
+  }
+
+  isLegalTarget(targetX: number, targetY: number): boolean {
+    return this.legalMoves.some(
+      (move) =>
+        move.toCoord.x === targetX &&
+        move.toCoord.y === targetY &&
+        move.fromCoord.x === this.selected.x &&
+        move.fromCoord.y === this.selected.y
+    );
+  }
+
+  isLegalSource(sourceX: number, sourceY: number): boolean {
+    return this.legalMoves.some(
+      (move) => move.fromCoord.x === sourceX && move.fromCoord.y === sourceY
+    );
   }
 
   drop($event: CdkDragDrop<{ row: number; col: number }>): void {
@@ -29,9 +78,14 @@ export class GameBoardComponent {
     const fromY = $event.item.dropContainer.data.col;
     const toX = $event.container.data.row;
     const toY = $event.container.data.col;
-    if (this.board?.isLegalMove(fromX, fromY, toX, toY)) {
-      const moveIndex = this.board?.getMoveIndex(fromX, fromY, toX, toY);
-      this.applyMoveEvent.emit(moveIndex);
-    }
+
+    const move: Move = {
+      fromCoord: { x: fromX, y: fromY },
+      toCoord: { x: toX, y: toY },
+      path: [],
+      index: -1,
+    };
+
+    this.gameService.applyMove(move);
   }
 }
--- webapp/src/app/services/game.service.ts
+++ webapp/src/app/services/game.service.ts
@@ -1,31 +1,98 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
-import { environment } from '../../environments/environment.development';
-import { GameIO } from '../types/game-io';
+import { Injectable, OnDestroy } from '@angular/core';
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  combineLatest,
+  filter,
+  map,
+  takeUntil,
+  withLatestFrom,
+} from 'rxjs';
+
 import { Game } from '../types/game.model';
+import { DataService } from './data.service';
+import { Move } from '../types/move.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GameService {
-  newGameUrl: string = environment.backendUrl + '/new-game';
-  applyMoveUrl: string = environment.backendUrl + '/apply-move';
+export class GameService implements OnDestroy {
+  private readonly onDestroy = new Subject<void>();
+  game$: Subject<Game>;
+  currentBoardIndex$: BehaviorSubject<number>;
+  moveToApply$: Subject<Move> = new Subject<Move>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private dataService: DataService) {
+    this.game$ = new Subject<Game>();
+    this.currentBoardIndex$ = new BehaviorSubject<number>(0);
+    this.currentBoardIndex$.next(0);
 
-  getNewGameBoard(): Observable<Game> {
-    return this.http
-      .post<GameIO>(this.newGameUrl, { size: 9, gameId: null })
-      .pipe(map((gameIo) => new Game(gameIo)));
+    this.dataService.getNewGame().subscribe((game: Game) => {
+      this.game$.next(game);
+    });
+
+    this.moveToApply$
+      .pipe(
+        takeUntil(this.onDestroy),
+        withLatestFrom(this.getLegalMoves(), this.game$),
+        map<[Move, Move[], Game], [Move[], Game]>(
+          ([moveToApply, legalMoves, game]) => [
+            legalMoves.filter((move) => {
+              return (
+                move.fromCoord.x === moveToApply.fromCoord.x &&
+                move.fromCoord.y === moveToApply.fromCoord.y &&
+                move.toCoord.x === moveToApply.toCoord.x &&
+                move.toCoord.y === moveToApply.toCoord.y
+              );
+            }),
+            game,
+          ]
+        ),
+        filter(([filteredMoves]) => {
+          return filteredMoves.length !== 0;
+        }),
+        map<[Move[], Game], [number, string]>(([move, game]) => {
+          return [move[0].index, game.gameId];
+        })
+      )
+      .subscribe(([moveIndex, gameId]) => {
+        dataService.applyMove(gameId, moveIndex).subscribe((game) => {
+          this.game$.next(game);
+          this.currentBoardIndex$.next(this.currentBoardIndex$.getValue() + 1);
+        });
+      });
   }
 
-  applyMove(gameId: string, moveIndex: number): Observable<Game> {
-    return this.http
-      .post<GameIO>(this.applyMoveUrl, {
-        gameId: gameId,
-        moveIndex: moveIndex,
+  ngOnDestroy(): void {
+    this.onDestroy.next();
+  }
+
+  applyMove(move: Move): void {
+    this.moveToApply$.next(move);
+  }
+
+  changeCurrentMoveIndex(newCurrentBoardIndex: number) {
+    this.currentBoardIndex$.next(newCurrentBoardIndex);
+  }
+
+  getCurrentBoardMatrix(): Observable<number[][]> {
+    return combineLatest([this.game$, this.currentBoardIndex$]).pipe(
+      map<[Game, number], number[][]>(([game, boardIndex]): number[][] => {
+        return game.boards[boardIndex].matrix;
       })
-      .pipe(map((gameIo) => new Game(gameIo)));
+    );
+  }
+
+  getLastMove(): Observable<Move> {
+    return this.game$.pipe(
+      map((game) => game.boards[game.boards.length - 1].lastMove)
+    );
+  }
+
+  getLegalMoves(): Observable<Move[]> {
+    return this.game$.pipe(
+      map((game) => game.boards[game.boards.length - 1].legalMoves)
+    );
   }
 }
