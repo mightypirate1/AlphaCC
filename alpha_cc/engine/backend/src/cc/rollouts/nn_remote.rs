b61f8ce7083f5cce0cd2298734186c2de8927316
Martin Frisk <yfflan@gmail.com>
2024-05-31 22:53:00 +0200

    make NNRemote panic if it doesnt get a response after 1 sec

--- alpha_cc/engine/backend/src/cc/rollouts/nn_remote.rs
+++ alpha_cc/engine/backend/src/cc/rollouts/nn_remote.rs
@@ -4,6 +4,9 @@ use crate::cc::pred_db::NNPred;
 use crate::cc::board::Board;
 
 
+const PATIENCE: u64 = 10000;
+const DELAY_NS: u64 = 100;
+
 pub struct NNRemote {
     pred_db: PredDBChannel,
 }
@@ -16,6 +19,7 @@ impl NNRemote {
     pub fn fetch_pred(&mut self, board: &Board) -> NNPred {
         // assumes there is a service running that will eventually
         // provide the prediction
+        let mut patience = PATIENCE;
         if self.pred_db.has_pred(board) {
             return self.pred_db.get_pred(board).unwrap();
         }
@@ -24,7 +28,11 @@ impl NNRemote {
             if self.pred_db.has_pred(board) {
                 return self.pred_db.get_pred(board).unwrap();
             }
-            thread::sleep(Duration::from_nanos(100));
+            thread::sleep(Duration::from_nanos(DELAY_NS));
+            patience -= 1;
+            if patience == 0 {
+                panic!("service not responding in {}ms", PATIENCE * DELAY_NS / 1_000);
+            }
         }
     }   
 }
