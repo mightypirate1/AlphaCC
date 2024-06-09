add9068f9d187657cbec23c0127f221eb3baaa21
Martin Frisk <yfflan@gmail.com>
2024-06-09 17:59:01 +0200

    NodeStore is now Board-centric instead of StateHash-centric

--- alpha_cc/agents/mcts/node_store/db_node_store.py
+++ alpha_cc/agents/mcts/node_store/db_node_store.py
@@ -4,7 +4,7 @@ from redis import Redis
 
 from alpha_cc.agents.mcts.mcts_node_py import MCTSNodePy
 from alpha_cc.agents.mcts.node_store.node_store import NodeStore
-from alpha_cc.state.game_state import StateHash
+from alpha_cc.engine import Board
 
 
 class DBNodeStore(NodeStore):
@@ -12,18 +12,23 @@ class DBNodeStore(NodeStore):
         self._game_id = game_id
         self._db = db
 
-    def __contains__(self, state_hash: StateHash) -> bool:
-        return bool(self._db.hexists(self.nodes_key, state_hash))  # type: ignore  # StateHash is bytes
-
-    def keys(self) -> list[StateHash]:
-        return self._db.hkeys(self.nodes_key)  # type: ignore
+    def __contains__(self, board: Board) -> bool:
+        return bool(self._db.hexists(self.nodes_key, hash(board)))  # type: ignore  # Board is bytes
 
     @property
     def nodes_key(self) -> str:
-        return f"games-db/nodes-cache/{self._game_id}"
+        return f"games-db/nodestore-nodes/{self._game_id}"
 
-    def get(self, state_hash: StateHash) -> MCTSNodePy:
-        encoded = self._db.hget(self.nodes_key, state_hash)  # type: ignore  # StateHash is bytes
+    @property
+    def boards_key(self) -> str:
+        return f"games-db/nodestore-boards/{self._game_id}"
+
+    def keys(self) -> list[Board]:
+        board_hashes = self._db.hkeys(self.nodes_key)
+        return [dill.loads(self._db.hget(self.boards_key, board_hash)) for board_hash in board_hashes]  # type: ignore  # noqa: S301
+
+    def get(self, board: Board) -> MCTSNodePy:
+        encoded = self._db.hget(self.nodes_key, hash(board))  # type: ignore
         if encoded is None:
             return MCTSNodePy(
                 pi=np.zeros(0, dtype=np.float32),
@@ -33,13 +38,16 @@ class DBNodeStore(NodeStore):
             )
         return dill.loads(encoded)  # noqa: S301
 
-    def set(self, state_hash: StateHash, node: MCTSNodePy) -> None:
-        encoded = dill.dumps(node)
-        self._db.hset(self.nodes_key, state_hash, encoded)  # type: ignore  # StateHash is bytes
+    def set(self, board: Board, node: MCTSNodePy) -> None:
+        encoded_board = dill.dumps(board)
+        encoded_node = dill.dumps(node)
+        self._db.hset(self.boards_key, hash(board), encoded_board)  # type: ignore
+        self._db.hset(self.nodes_key, hash(board), encoded_node)  # type: ignore
 
     def clear(self) -> None:
+        self._db.delete(self.boards_key)
         self._db.delete(self.nodes_key)
 
     def load_from(self, node_store: NodeStore) -> None:
-        for state_hash in node_store.keys():  # noqa
-            self.set(state_hash, node_store.get(state_hash))
+        for board in node_store.keys():  # noqa
+            self.set(board, node_store.get(board))
--- alpha_cc/agents/mcts/node_store/node_store.py
+++ alpha_cc/agents/mcts/node_store/node_store.py
@@ -4,24 +4,24 @@ from abc import ABC, abstractmethod
 from collections.abc import Iterable
 
 from alpha_cc.agents.mcts.mcts_node_py import MCTSNodePy
-from alpha_cc.state.game_state import StateHash
+from alpha_cc.engine import Board
 
 
 class NodeStore(ABC):
     @abstractmethod
-    def __contains__(self, state_hash: StateHash) -> bool:
+    def __contains__(self, board: Board) -> bool:
         pass
 
     @abstractmethod
-    def keys(self) -> Iterable[StateHash]:
+    def keys(self) -> Iterable[Board]:
         pass
 
     @abstractmethod
-    def get(self, state_hash: StateHash) -> MCTSNodePy:
+    def get(self, board: Board) -> MCTSNodePy:
         pass
 
     @abstractmethod
-    def set(self, state_hash: StateHash, node: MCTSNodePy) -> None:
+    def set(self, board: Board, node: MCTSNodePy) -> None:
         pass
 
     @abstractmethod
