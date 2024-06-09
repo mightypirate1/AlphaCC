84bf78ccb4a08849c343ee1d2404894817669155
lucas <kylberg@gmail.com>
2024-06-09 16:17:45 +0200

    add barchart component

---
+++ webapp/src/app/components/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import { NgxEchartsDirective } from 'ngx-echarts';
+
+@Component({
+  selector: 'app-bar-chart',
+  standalone: true,
+  imports: [NgxEchartsDirective],
+  templateUrl: './bar-chart.component.html',
+  styleUrl: './bar-chart.component.scss',
+})
+export class BarChartComponent implements OnInit {
+  @Input() title: string = '';
+  @Input()
+  set data(data: number[]) {
+    this.mergeOptions = {
+      series: [{ data: data }],
+    };
+  }
+  initOpts = {
+    renderer: 'svg',
+    width: 350,
+    height: 225,
+  };
+  mergeOptions: EChartsOption = {};
+  options: EChartsOption = {};
+
+  ngOnInit(): void {
+    this.options = {
+      title: { text: this.title },
+      legend: {},
+      tooltip: {},
+
+      xAxis: { type: 'category' },
+      yAxis: {},
+
+      series: [
+        {
+          type: 'bar',
+        },
+      ],
+    };
+  }
+}
