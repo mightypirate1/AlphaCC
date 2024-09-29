b7d84a7ee1ddbb3f5cc32201a522d65d655169fa
Martin Frisk <yfflan@gmail.com>
2024-09-29 12:46:57 +0200

    fix arg-max-delay logic

--- alpha_cc/runtimes/training_runtime.py
+++ alpha_cc/runtimes/training_runtime.py
@@ -25,10 +25,12 @@ class TrainingRunTime:
         rollout_depth: int | None = None,
         action_temperature: float = 1.0,
         max_game_length: int | None = None,
+        argmax_delay: int | None = None,
     ) -> list[MCTSExperience]:
         board = self._board.reset()
         agent = self._agent
         max_game_duration = np.inf if max_game_length is None else max_game_length
+        time_to_argmax = argmax_delay if argmax_delay is not None else np.inf
         agent.on_game_start()
 
         trajectory: list[MCTSExperience] = []
@@ -47,7 +49,11 @@ class TrainingRunTime:
                     v_target=value,
                 )
                 trajectory.append(experience)
-                a = np.random.choice(len(pi), p=pi)
+
+                a = int(np.argmax(pi))
+                if (time_to_argmax := time_to_argmax - 1) >= 0:
+                    a = np.random.choice(len(pi), p=pi)
+
                 moves = board.get_moves()
                 board = board.apply(moves[a])
                 pbar.update(1)
