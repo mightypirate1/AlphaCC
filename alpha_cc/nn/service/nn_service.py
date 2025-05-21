1729c0204d93d9247d91cdd56bfd6a5b29a503e4
Martin Frisk <martin@abstraction.dev>
2025-05-21 12:49:13 +0200

    improved efficiency and robustness

--- alpha_cc/nn/service/nn_service.py
+++ alpha_cc/nn/service/nn_service.py
@@ -1,11 +1,11 @@
 import logging
 from collections.abc import Callable
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 
 import torch
 from apscheduler.job import Job
 from apscheduler.schedulers.background import BackgroundScheduler
-
-from concurrent.futures import ThreadPoolExecutor
 
 from alpha_cc.db import TrainingDB
 from alpha_cc.engine import NNPred, PredDBChannel
@@ -22,41 +22,45 @@ class ServedNN:
         self,
         pred_db_channel: PredDBChannel,
         training_db: TrainingDB,
+        nn_creator: Callable[[], torch.nn.Module],
         nn: torch.nn.Module,
+        current_weights_index: int,
         inference_batch_size: int = 512,
         num_post_workers: int = 2,
         log_frequency: int = 60,
-        reload_frequency: int = 1,
         device: str = "cpu",
     ) -> None:
         self._pred_db_channel = pred_db_channel
         self._training_db = training_db
+        self._nn_creator = nn_creator
+        self._current_weights_index = current_weights_index
         self._inference_batch_size = inference_batch_size
         self._log_frequency = log_frequency
-        self._reload_frequency = reload_frequency
         self._device = torch.device(device)
-        self._nn: torch.nn.Module | None = None
-        self._current_weights_index = -1
         self._n_preds = 0
         self._n_batches = 0
+        self._nn_prediction = self._configure_nn(nn)  # served nn
+        self._nn_loading = self._configure_nn(nn)  # used for loading weights
         self._jobs = self._initialize_scheduler()
-        self._assign_nn(nn)
         self._post_pool = ThreadPoolExecutor(max_workers=num_post_workers)
+        self._is_loading_weights = False
         pred_db_channel.flush_preds()
 
     @property
     def channel(self) -> int:
         return self._pred_db_channel.channel
 
+    @property
+    def current_weights_index(self) -> int:
+        return self._current_weights_index
+
     def process_requests(self) -> None:
-        if self._nn is None:
-            raise ValueError(f"can not call a deactivated ServedNN[channel={self.channel}]")
-        boards = self._pred_db_channel.fetch_all()
+        boards = self._pred_db_channel.fetch_all_requests()
         if len(boards) == 0:
             return
         states = [GameState(board) for board in boards]
         x = self._prepare_input(states)
-        
+
         # batch it up, in case we have a LOT of requests
         for i in range(0, len(states), self._inference_batch_size):
             batch_states = states[i : i + self._inference_batch_size]
@@ -66,28 +70,52 @@ class ServedNN:
                 continue
 
             with torch.no_grad(), torch.cuda.amp.autocast(enabled=self._device == "cuda"):
-                x_batch_pis, x_batch_vals = self._nn(x_batch)
+                x_batch_pis, x_batch_vals = self._nn_prediction(x_batch)
 
             # post in a separate thread to avoid blocking GPU
             self._post_pool.submit(
                 self._post_predictions,
-                    batch_states,
-                    x_batch_pis.detach().to("cpu", non_blocking=True),
-                    x_batch_vals.detach().to("cpu", non_blocking=True),
-                )
+                batch_states,
+                x_batch_pis.detach().cpu(),
+                x_batch_vals.detach().cpu(),
+            )
 
         self._n_preds += len(states)
         self._n_batches += 1
 
     def deactivate(self) -> None:
-        self._nn = None
         for job in self._jobs:
             job.remove()
 
-    def activate(self, nn: torch.nn.Module) -> None:
-        self._nn = nn
-        self._pred_db_channel.flush_preds()
-        self._jobs = self._initialize_scheduler()
+    def load_weights(self, target_weight_index: int) -> None:
+        if target_weight_index == self._current_weights_index or self._is_loading_weights:
+            return
+
+        self._is_loading_weights = True
+
+        def background_loading() -> None:
+            try:
+                # get weights from the training db into the loading nn
+                weights = self._training_db.weights_fetch(target_weight_index)
+                self._nn_loading.load_state_dict(weights)
+                # swap the loading nn with the prediction nn
+                old_model = self._nn_prediction
+                self._nn_prediction = self._nn_loading
+                self._nn_loading = old_model
+
+                self._pred_db_channel.flush_preds()
+                logger.info(
+                    f"[Channel-{self._pred_db_channel.channel}]:"
+                    f" updated weights {self._current_weights_index}->{target_weight_index}"
+                )
+                self._current_weights_index = target_weight_index
+                torch.cuda.empty_cache()
+            except Exception as e:  # noqa: BLE001
+                logger.error(f"Failed to load weights: {e}")
+            finally:
+                self._is_loading_weights = False
+
+        Thread(target=background_loading).start()
 
     def _prepare_input(self, states: list[GameState]) -> torch.Tensor:
         """assumes at least one state is passed"""
@@ -104,6 +132,7 @@ class ServedNN:
 
         They are assumed to be on the CPU, and in the same order as the states.
         """
+
         def create_nn_pred(state: GameState, pi_tensor_unsoftmaxed: torch.Tensor, val: torch.Tensor) -> NNPred:
             pi_unsoftmaxed = pi_tensor_unsoftmaxed[*action_indexer(state)]
             pi = torch.nn.functional.softmax(pi_unsoftmaxed, dim=0)
@@ -116,17 +145,16 @@ class ServedNN:
         for state, nn_pred in zip(states, nn_preds):
             self._pred_db_channel.post_pred(state.board, nn_pred)
 
-    def _assign_nn(self, nn: torch.nn.Module) -> None:
-        self._nn = nn
+    def _configure_nn(self, nn: torch.nn.Module) -> torch.nn.Module:
         # TODO: make jit-compilation work
-        # self._nn = torch.jit.script(nn)
-        self._nn.to(self._device)
-        self._nn.eval()
+        # nn = torch.jit.script(nn)
+        nn = nn.to(self._device)
+        nn = nn.eval()
+        return nn
 
     def _initialize_scheduler(self) -> list[Job]:
         stats_job = scheduler.add_job(self._log_stats, "interval", seconds=self._log_frequency)
-        weights_job = scheduler.add_job(self._update_weights, "interval", seconds=self._reload_frequency)
-        return [stats_job, weights_job]
+        return [stats_job]
 
     def _log_stats(self) -> None:
         logger.info(
@@ -136,25 +164,6 @@ class ServedNN:
         self._n_preds = 0
         self._n_batches = 0
 
-    def _update_weights(self) -> None:
-        if self._nn is None:
-            logger.warning(f"[Channel-{self._pred_db_channel.channel}]: attempted reload but is deactivated")
-            return
-        target_weight_index = self._training_db.model_get_current().get(self.channel)
-        if target_weight_index is None:
-            logger.warning(f"[Channel-{self._pred_db_channel.channel}]: attempted reload but has no target weights")
-            return
-
-        if target_weight_index == self._current_weights_index:
-            return
-
-        self._current_weights_index = target_weight_index
-        weights = self._training_db.weights_fetch(target_weight_index)
-        self._nn.load_state_dict(weights)
-        self._nn.eval()
-        self._pred_db_channel.flush_preds()
-        logger.info(f"[Channel-{self._pred_db_channel.channel}]: updated weights to {target_weight_index}")
-
 
 class NNService:
     def __init__(
@@ -163,7 +172,7 @@ class NNService:
         redis_host_main: str = "localhost",
         redis_host_pred: str = "localhost",
         log_frequency: int = 60,
-        reload_frequency: int = 1,
+        reload_frequency: int = 5,
         infecence_batch_size: int = 512,
         num_post_workers: int = 2,
         gpu: bool = False,
@@ -187,12 +196,19 @@ class NNService:
             for served_nn in self._served_nns:
                 served_nn.process_requests()
 
-    def add_channel(self, channel: int) -> None:
+    def add_channel(self, channel: int, weight_index: int) -> None:
         for served_nn in self._served_nns:
             if served_nn.channel == channel:
                 logger.warning(f"[Channel-{channel}]: already exists")
                 return
-        self._served_nns.append(self._create_served_nn(channel))
+        self._served_nns.append(self._create_served_nn(channel, weight_index))
+
+    def get_served_nn_by_channel(self, channel: int) -> ServedNN | None:
+        for served_nn in self._served_nns:
+            if served_nn.channel == channel:
+                return served_nn
+        logger.warning(f"[Channel-{channel}]: not found")
+        return None
 
     def drop_channel(self, channel: int) -> None:
         for served_nn in self._served_nns:
@@ -205,28 +221,38 @@ class NNService:
     def has_channel(self, channel: int) -> bool:
         return any(snn.channel == channel for snn in self._served_nns)
 
-    def _create_served_nn(self, channel: int) -> ServedNN:
+    def _create_served_nn(self, channel: int, weight_index: int) -> ServedNN:
         pred_db_channel = PredDBChannel(self._redis_host_pred, channel)
         nn = self._nn_creator()
-        return ServedNN(
+        served_nn = ServedNN(
             pred_db_channel,
             self._training_db,
+            self._nn_creator,
             nn,
+            weight_index,
             inference_batch_size=self._infecence_batch_size,
             num_post_workers=self._num_post_workers,
             log_frequency=self._log_frequency,
-            reload_frequency=self._reload_frequency,
             device=self._device,
         )
+        served_nn.load_weights(weight_index)
+        return served_nn
 
     def _initialize_scheduler(self) -> None:
         def update_served_nets() -> None:
-            current_channels = self._training_db.model_get_current().keys()
-            for channel in current_channels:
+            current_models = self._training_db.model_get_current()  # what the trainer is telling us to serve
+            for channel, weight_idx in current_models.items():
                 if not self.has_channel(channel):
-                    self.add_channel(channel)
+                    self.add_channel(channel, weight_idx)
+                else:
+                    channel_nn = self.get_served_nn_by_channel(channel)
+                    if channel_nn is not None:
+                        channel_nn.load_weights(weight_idx)
+                    else:
+                        logger.warning(f"[Channel-{channel}]: not found in served nns")
+
             for served_nn in self._served_nns:
-                if served_nn.channel not in current_channels:
+                if served_nn.channel not in current_models:
                     self.drop_channel(served_nn.channel)
 
         scheduler.add_job(update_served_nets, "interval", seconds=self._reload_frequency)
